1292653d73d9174c3b6c649778192f6eb6974355
Sarah <sarahparr92@gmail.com>
2020-03-02 12:42:39 -0800

    Revised Files for Drill 104
    Revised Files for Drill 104 So Only .Txt Files are Inserted into DB and Printed

--- Drill 104/test.py
+++ Drill 104/test.py
@@ -17,27 +17,13 @@ conn = sqlite3.connect('test.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('information.docx',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('Hello.txt',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myImage.png',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myMovie.mpg',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('World.txt',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('data.pdf',))
-    cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
-                ('myPhoto.jpg',))
+    
+    fileList = ('information.docx','Hello.txt','myImage.png', \
+            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
+    for fileName in fileList:
+        if fileName.endswith('.txt'):
+            cur.execute("INSERT INTO tbl_files(col_fname) VALUES (?)", \
+                (fileName,))
+            print(fileName)
     conn.commit()
 conn.close()
-
-conn = sqlite3.connect('test.db')
-
-with conn:
-    cur = conn.cursor()
-    cur.execute("SELECT col_fname FROM tbl_files WHERE col_fname like '%.txt'")
-    varFile = cur.fetchall()
-    print (varFile)
